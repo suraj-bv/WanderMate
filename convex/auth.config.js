539ba301b9dcafe03037caddc3bb1fac66402f34
suraj-bv <surajbv5566@gmail.com>
2026-04-13 15:27:00 +0530

    Redeploying 2....

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://witty-albacore-30.clerk.accounts.dev",
+      domain: "https://accounts.wandermate.surajbv.me",
       applicationID: "convex",
     },
   ],
